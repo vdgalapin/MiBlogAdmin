68ddc0d1e7fcc82196dd2bc553dfa1532b5f8d38
Vyron Galapin <VGalapin@KMMMVL.local>
2023-03-28 09:02:49 -0500

    testing theory

--- __init__.py
+++ __init__.py
@@ -45,7 +45,7 @@ def create_app():
     db.init_app(app)
     with app.app_context():
         db.create_all()
-        print('CREATE DATABASE!')
+        print('CREATE DATABASE')
     app.app_context().push()
     
     @login_manager.user_loader
