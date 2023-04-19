25fd5b4b7758ebbbf6114977ee73873651a30a16
Vyron Galapin <VGalapin@KMMMVL.local>
2023-04-19 07:05:32 -0500

    add react

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,20 +1,20 @@
-class Footer extends HTMLElement {
-    constructor() {
-      super();
-    }
+// class Footer extends HTMLElement {
+//     constructor() {
+//       super();
+//     }
   
-    connectedCallback() {
-      this.innerHTML = `
-        <style>
+//     connectedCallback() {
+//       this.innerHTML = `
+//         <style>
 
-        </style>
-        <footer class="footer mt-auto py-3 bg-light">
-            <div class="container">
-                <a href="#main_header">Back to top</a>
-            </div>
-        </footer>
-      `;
-    }
-  }
+//         </style>
+//         <footer class="footer mt-auto py-3 bg-light">
+//             <div class="container">
+//                 <a href="#main_header">Back to top</a>
+//             </div>
+//         </footer>
+//       `;
+//     }
+//   }
   
-  customElements.define('footer-component', Footer);
+//   customElements.define('footer-component', Footer);
