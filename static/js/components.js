e71a6558e7d8b343a8e7ab5fdc232031ffe8186e
Vyron Galapin <VGalapin@KMMMVL.local>
2023-04-19 07:40:33 -0500

    class component

--- static/js/components.js
+++ static/js/components.js
@@ -70,38 +70,49 @@
   
 //   customElements.define('header-component', Header);
 
-function HeaderBar() {
-  return <header class="" id="main_header">
+function LogoHead() {
+  return (
+      <a href="/" class="nav-link px-2 link-secondary" >
+        <img src="../static/images/Dapper.gif" />
+      </a>
+  );
+}
+
+// Using a class component 
+class HeaderBar extends React.Component {
+  render() { return (<header class="" id="main_header">
               <div class="container">
                 <div class="row">
                   <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start col-6 col-md-3 col-sm-4">
                       <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                           <li>
-                            <a href="/" class="nav-link px-2 link-secondary" >
-                              <img src="../static/images/Dapper.gif" />
-                            </a>
+                              <LogoHead />
                           </li>
                       </ul>
                   </div>
                   <div class="col-1 col-md-6 col-sm-4">
                   </div>
                   <div class="col-5 col-md-3 col-sm-4 text-left">
-                
                   </div>
                 </div>
               </div>
-          </header>;
+          </header>
+    );
+  }
 }
 
+
+// Using a functional component
 function FooterBar() {
-  return  <footer class="footer mt-auto py-3 bg-light">
+  return ( <footer class="footer mt-auto py-3 bg-light">
             <div class="container">
                 <a href="#main_header">Back to top</a>
             </div>
-          </footer>;
+          </footer>);
 }
 
 
 // headerBar does not work the first letter to be capitalize
-ReactDOM.render(HeaderBar(), document.getElementById('header'));
-ReactDOM.render(FooterBar(), document.getElementById('footer'));
+ReactDOM.render(<HeaderBar />, document.getElementById('header'));
+ReactDOM.render(<FooterBar />, document.getElementById('footer'));
+
